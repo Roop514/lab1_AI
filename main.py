f9db6ffef1941e58635de24cb461462bbfead706
jammesss <157440523+jammesss@users.noreply.github.com>
2026-09-15 15:55:49 -0600

    replaced old ones to new codes

---
+++ main.py
@@ -0,0 +1,31 @@
+"""
+Your name(s): 
+"""
+
+
+from nodes import WikiPage
+
+# change the start and destination pages as you like - see if your algorithm can find a route between them
+start = WikiPage('https://en.wikipedia.org/wiki/Mount_Royal_University')
+goal = WikiPage('https://en.wikipedia.org/wiki/Artificial_intelligence')
+
+
+
+# What search algorithm are you using? Why?
+# (your response here)
+
+
+
+# Does your code work? (Verify the route found by your search algorithm)
+
+
+
+
+# YOUR SEARCH ALGORITHM CODE HERE
+# (print the discovered route when you find it)
+
+
+
+
+# Can you think of a search approach that could be faster than your implementation above?
+# Describe it.
